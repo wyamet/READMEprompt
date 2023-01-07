c765c66130e9498755c0a4245b4d2d4dc71e331c
wyamet <wyattdomanski.md@gmail.com>
2023-01-06 18:30:08 -0800

    created an array of questions for the user input

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+async function generateReadme() {
+    const questions = [
+        {
+          type: 'input',
+          name: 'projectName',
+          message: 'What is the name of your project?'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Please provide a short description of your project:'
+        },
+        {
+          type: 'input',
+          name: 'installationStep1',
+          message: 'What is step 1 of the installation instructions for your project:'
+        },
+        {
+            type: 'input',
+            name: 'installationStep2',
+            message: 'What is step 2 of the installation instructions for your project:'
+          },
+          {
+            type: 'input',
+            name: 'installationStep3',
+            message: 'What is step 3 of the installation instructions for your project:'
+          },
+        {
+          type: 'input',
+          name: 'usageInstructions',
+          message: 'Please provide usage instructions for your project:'
+        },
+        {
+          type: 'input',
+          name: 'contributionGuidelines',
+          message: 'Please provide contribution guidelines for your project:'
+        },
+        {
+          type: 'input',
+          name: 'testInstructions',
+          message: 'Please provide test instructions for your project:'
+        },
+      ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
